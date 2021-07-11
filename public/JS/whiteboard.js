1c7c0afe34475a71132bcff98e48957f8404c5d3
Abhilasha Bansal <64535297+AbhilashaBansal@users.noreply.github.com>
2021-07-12 02:54:22 +0530

    trynna fix whiteboard drawing & alignment

--- public/JS/whiteboard.js
+++ public/JS/whiteboard.js
@@ -1,5 +1,6 @@
 // let socket = io();
 
+// canvas element
 let canvas = $("canvas")[0];
 let colors = $(".color");
 let clearb = $("#clear");
@@ -7,6 +8,7 @@ let eraser = $("#eraser");
 
 let context = canvas.getContext('2d');
 
+// background color
 context.fillStyle = "white";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -35,10 +37,10 @@ socket.on('drawing', onDrawingEvent);
 
 // window.addEventListener('resize', onResize, false);
 // onResize();
-
+// $("canvas").attr("width", $("#wb").width());
 
 function drawLine(x0, y0, x1, y1, color, emit){
-  console.log(x0, y0, x1, y1);
+  // console.log(x0, y0, x1, y1);
   context.beginPath();
   context.moveTo(x0, y0);
   context.lineTo(x1, y1);
@@ -61,24 +63,26 @@ function drawLine(x0, y0, x1, y1, color, emit){
 }
 
 
+// Event Listeners for Drawing
 function onMouseDown(e){
-  let canvasOffset=$("#canvas").position();
-  let offsetX=canvasOffset.left;
-  let offsetY=canvasOffset.top;
+  // let canvasOffset = $("#canvas").offset();
+  let canvasOffset = $(".whiteboard").offset();
+  let offsetX = canvasOffset.left;
+  let offsetY = canvasOffset.top;
   console.log(offsetX, offsetY);
 
   drawing = true;
-  // let pos = getMousePos(canvas, e);
   current.x = parseInt(e.clientX-offsetX);
-  // current.x = e.clientX||e.touches[0].clientX;
   current.y = parseInt(e.clientY-offsetY);
+  // current.x = e.clientX||e.touches[0].clientX;
+  
   console.log(current);
 }
 
 function onMouseUp(e){
-  let canvasOffset=$("#canvas").position();
-  let offsetX=canvasOffset.left;
-  let offsetY=canvasOffset.top;
+  let canvasOffset = $(".whiteboard").offset();
+  let offsetX = canvasOffset.left;
+  let offsetY = canvasOffset.top;
   console.log(offsetX, offsetY);
 
   if (!drawing) {return;}
@@ -90,9 +94,11 @@ function onMouseUp(e){
 
 function onMouseMove(e){
   if (!drawing) { return; }
-  let canvasOffset=$("#canvas").position();
-  let offsetX=canvasOffset.left;
-  let offsetY=canvasOffset.top;
+
+  // let canvasOffset = $("#canvas").offset();
+  let canvasOffset = $(".whiteboard").offset();
+  let offsetX = canvasOffset.left;
+  let offsetY = canvasOffset.top;
   let x = parseInt(e.clientX-offsetX);
   let y = parseInt(e.clientY-offsetY);
   
@@ -101,11 +107,13 @@ function onMouseMove(e){
   current.y = y;
 }
 
+
 function onColorUpdate(e){
   current.color = e.target.className.split(' ')[1];
 }
 
-// limit the number of events per second
+
+//to limit the number of events per second
 function throttle(callback, delay) {
   let previousCall = new Date().getTime();
   return function() {
@@ -118,12 +126,15 @@ function throttle(callback, delay) {
   };
 }
 
+// socket event, collaborative whiteboard isi se banta hai
 function onDrawingEvent(data){
   let w = canvas.width;
   let h = canvas.height;
   drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
 }
 
+
+// review later
 // make the canvas fill its parent
 function onResize() {
   let ww = $("#canvas").parent().width();
